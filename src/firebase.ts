bedadeec2e7ce402c1f19ce7ff6b7287f344b457
Sanka <sankasanjeevadg@gmail.com>
2024-03-16 18:54:24 +0530

    update app title

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,13 +1,13 @@
 import { initializeApp } from 'firebase/app'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBDPVeWyaAQ5-2kzSsz0Nm72jMKt1cfUXw',
-  authDomain: 'schoolsync-3d8e6.firebaseapp.com',
-  projectId: 'schoolsync-3d8e6',
-  storageBucket: 'schoolsync-3d8e6.appspot.com',
-  messagingSenderId: '893603407536',
-  appId: '1:893603407536:web:c67716cf2c8d279e8b7d75',
-  measurementId: 'G-PBRQZLKSYF',
+  apiKey: import.meta.env.VITE_APP_API_KEY,
+  authDomain: import.meta.env.VITE_APP_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_APP_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_APP_STORAGE_BUCKER,
+  messagingSenderId: import.meta.env.VITE_APP_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_APP_ID,
+  measurementId: import.meta.env.VITE_APP_MEASUREMENT_ID,
 }
 
 // eslint-disable-next-line import/prefer-default-export
