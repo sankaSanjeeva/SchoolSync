9c91f7626f14d8029c64c3ba8e4ad4984c92a248
Sanka <sankasanjeevadg@gmail.com>
2024-03-22 20:19:30 +0530

    user sign in logic

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,20 +1,61 @@
+import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { signInWithPopup } from 'firebase/auth'
-import { auth, provider } from '@/firebase'
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth'
+import { getAdditionalUserInfo, onAuthStateChanged } from 'firebase/auth'
+import { doc, setDoc } from 'firebase/firestore'
+import { auth, db } from '@/firebase'
 import { Button } from '@/components/ui/button'
+import { Role } from '@/enums'
+import { SpinnerIcon } from '@/assets/icons'
 
 export default function Auth() {
+  const [loading, setLoading] = useState(false)
+
   const navigate = useNavigate()
 
-  const handleSignUp = () => {
-    signInWithPopup(auth, provider).then(() => {
-      navigate('/chat')
-    })
+  const [signInWithGoogle, , , error] = useSignInWithGoogle(auth)
+
+  const handleSignUp = async () => {
+    setLoading(true)
+
+    const userCredential = await signInWithGoogle()
+
+    const user = getAdditionalUserInfo(userCredential!)
+
+    if (user?.isNewUser) {
+      await setDoc(doc(db, 'users', user?.profile?.id as string), {
+        name: user?.profile?.name,
+        email: user?.profile?.email,
+        picture: user?.profile?.picture,
+        role: Role.User,
+      })
+    }
+
+    setLoading(false)
   }
+
+  useEffect(() => {
+    if (error) {
+      setLoading(false)
+    }
+  }, [error])
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        navigate('/chat')
+      }
+    })
+
+    return () => unsubscribe()
+  }, [navigate])
 
   return (
     <div className="flex justify-center items-center h-screen">
-      <Button onClick={handleSignUp}>Sign up with google</Button>
+      <Button disabled={loading} onClick={handleSignUp}>
+        {loading && <SpinnerIcon className="animate-spin mr-3" />}
+        <span>Sign in with google</span>
+      </Button>
     </div>
   )
 }
